daa743f0bfbc6c92a182052119098f22ef233f8b
jiang.junmin <409850956@qq.com>
2021-11-30 00:24:08 +0800

    使用wire插件来生成proto对应的java文件，添加这些并sync项目后可以在gradle任务列表中找到generateProtos任务，可以主动运行以生成文件（编译时运行也可以生成）

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wire.demo.lib.SomeText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val sss = SomeText("")
+        sss.encodeByteString()
     }
 }
